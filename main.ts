3f4265efe8ab4ecfb5294befa1a0c85b02701654
urnatko <92942456+urnatko@users.noreply.github.com>
2021-10-25 22:00:20 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+let cislo = 0
+input.onButtonPressed(Button.A, function () {
+    cislo = 10
+    for (let index = 0; index < 10; index++) {
+        basic.showNumber(cislo)
+        cislo += -1
+    }
+    basic.showIcon(IconNames.Skull)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.B, function () {
+    cislo = 10
+    while (cislo >= 0) {
+        basic.showNumber(cislo)
+        cislo += -1
+    }
+    basic.showIcon(IconNames.Skull)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
+basic.forever(function () {
+	
+})
